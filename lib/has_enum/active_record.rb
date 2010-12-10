2d94aff89fb934834856e24d7465507a5fc61f6f
Konstantin Shabanov <kes.eclipse@gmail.com>
2010-12-10 22:49:09 +0600

    whitespace clean

--- lib/has_enum/active_record.rb
+++ lib/has_enum/active_record.rb
@@ -4,22 +4,22 @@ module HasEnum
       base.write_inheritable_hash(:enum, {})
       base.extend(ClassMethods)
     end
-    
+
     module ClassMethods
       def enum(attribute = nil)
         @enum ||= read_inheritable_attribute(:enum)
         attribute ? @enum[attribute.to_sym] : @enum
       end
-      
+
       def has_enum(attribute, values, options = {})
         enum[attribute] = values.freeze
-        
+
         if options[:query_methods] != false
           values.each do |val|
             define_method(:"#{val}?") { self.send(attribute) == val }
           end
         end
-        
+
         define_method(:"#{attribute}=") do |value|
           if value.nil? or values.find{ |val| val == value }
             set_value  = lambda { |value| value.is_a?(Symbol) ? Marshal.dump(value) : value }
@@ -28,13 +28,13 @@ module HasEnum
             errors.add(:"#{attribute}", "#{value} is not in enum")
           end
         end
-        
+
         define_method(:"#{attribute}") do
           load_value = lambda { |value| value[0].getbyte(0) == 4 ? Marshal.load(value) : value }
           value = self[:"#{attribute}"]
           value ? load_value.call(value) : nil
         end
-        
+
         define_method "human_#{attribute}" do
           begin
             return nil unless self.send(attribute)
@@ -45,7 +45,7 @@ module HasEnum
           end
         end
       end
-      
+
       def values_for_select_tag(enum)
         values = enum(enum)
         begin

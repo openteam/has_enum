432293b786e820bf6d17f097d0f660647d4ac7c1
Dmitry Lihachev <lda@openteam.ru>
2011-03-24 12:36:05 +0600

    extend formtastic on initialize

--- lib/has_enum.rb
+++ lib/has_enum.rb
@@ -13,3 +13,28 @@ class ActiveRecord::Base
   include HasEnum
 end
 
+begin
+  require 'formtastic'
+
+  Formtastic::SemanticFormBuilder.class_eval do
+
+    def enum_input(method, options = {})
+      value = @object.send(method)
+      options.reverse_merge! :as => :select,
+                             :collection => @object.class.human_enums[method].invert.to_a
+      self.input(method, options).gsub(/class="select/, 'class="enum')
+    end
+
+    def input_with_enum(method, options={})
+      if @object.class.respond_to?(:enums) && @object.class.enums[method] && !options[:as]
+        enum_input(method, options)
+      else
+        input_without_enum(method, options)
+      end
+    end
+
+    alias_method_chain :input, :enum
+
+  end
+rescue LoadError
+end

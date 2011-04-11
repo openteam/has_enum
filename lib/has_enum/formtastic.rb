2aadd9467b0b6be5bdba4fab1f566f24912a11a2
Dmitry Lihachev <lda@openteam.ru>
2011-04-11 17:07:19 +0700

    formtastic support for multiple enum

--- lib/has_enum/formtastic.rb
+++ lib/has_enum/formtastic.rb
@@ -1,9 +1,12 @@
 Formtastic::SemanticFormBuilder.class_eval do
 
   def enum_input(method, options = {})
-    options.merge! :collection => object.class.values_for_select_tag(method),
-                   :wrapper_html => {:class => :enum}
-    select_input method, options
+    options.merge! :collection => object.class.values_for_select_tag(method)
+    if object.class.has_mutliple_enum? method
+      check_boxes_input method, options
+    else
+      select_input method, options.merge(:wrapper_html => {:class => :enum})
+    end
   end
 
   def default_input_type_with_enum(method, options={})
